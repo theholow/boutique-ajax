6408de22439969d11556b94ccbf5f5d6835ab6e1
unknown <owen.guerin121@gmail.com>
2020-10-20 19:03:53 +0200

    Affichage produit page d'accueil

---
+++ frontend/axios.js
@@ -0,0 +1,39 @@
+function createDisplay(array) {
+    for (let product of array) {
+        let name = product.name
+        let img = product.imageUrl
+        let price = product.price
+        let descritption = product.description
+        document.getElementById('card').innerHTML += createCard(name,descritption,price,img)
+
+    }
+}
+
+function createCard(name,description,price,img){
+return '<div class="col-md-4 mb-5">' +
+    '<div class="card h-100">' +
+    '<img class="card-img-top" src="' +img+'" alt="">' +
+    '<div class="card-body">' +
+    '<h4 class="card-title">' + name + '</h4>'
+    +
+    '<p class="card-text">' + description + '</p>'
+    +
+    '<div class="card-footer">'+
+        '<a href="#" class="btn btn-warning">Achetez moi!</a>'+
+    '<p>'+ price +'</p>'+
+    '</div>'
+    +
+    '</div>' +
+    '</div>' +
+    '</div>'
+}
+
+const getData = () => {
+    axios('http://localhost:3000/api/teddies')
+        .then(response=>{
+            createDisplay(response.data);
+            console.log(response.data[0].imageUrl)
+        })
+}
+
+getData();
