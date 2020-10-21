6582041e4c44fe3167fd32b8a8702490b7734b5f
oriana13 <oriana.pfalz13@gmail.com>
2020-10-21 14:16:58 +0200

    affichage d'un produit avec ajax (axios)

--- frontend/axios.js
+++ frontend/axios.js
@@ -1,39 +1,82 @@
+const getData = () => {
+    axios('http://localhost:3000/api/teddies')
+        .then(response => {
+            createDisplay(response.data)
+        })
+}
+getData();
+
+
 function createDisplay(array) {
     for (let product of array) {
+        let id = product._id
         let name = product.name
         let img = product.imageUrl
         let price = product.price
         let descritption = product.description
-        document.getElementById('card').innerHTML += createCard(name,descritption,price,img)
+        document.getElementById('card').innerHTML += createCard(id, name, descritption, price, img)
 
     }
 }
 
-function createCard(name,description,price,img){
-return '<div class="col-md-4 mb-5">' +
-    '<div class="card h-100">' +
-    '<img class="card-img-top" src="' +img+'" alt="">' +
-    '<div class="card-body">' +
-    '<h4 class="card-title">' + name + '</h4>'
-    +
-    '<p class="card-text">' + description + '</p>'
-    +
-    '<div class="card-footer">'+
-        '<a href="#" class="btn btn-warning">Achetez moi!</a>'+
-    '<p>'+ price +'</p>'+
-    '</div>'
-    +
-    '</div>' +
-    '</div>' +
-    '</div>'
+
+function createCard(id, name, description, price, img) {
+    return '<div class="col-md-4 mb-5">' +
+        '<div class="card h-100">' +
+        '<img class="card-img-top" src="' + img + '" alt="">' +
+        '<div class="card-body">' +
+        '<h4 class="card-title">' + name + '</h4>'
+        +
+        '<p class="card-text">' + description + '</p>'
+        +
+        '<div class="card-footer">' +
+        '<a href="../produit/index.html?id=' + id + '" class="btn btn-warning">Achetez moi!</a>' +
+        '<p>' + price + '</p>' +
+        '</div>'
+        +
+        '</div>' +
+        '</div>' +
+        '</div>'
 }
 
-const getData = () => {
-    axios('http://localhost:3000/api/teddies')
-        .then(response=>{
-            createDisplay(response.data);
-            console.log(response.data[0].imageUrl)
+
+let urlcourante = document.location.href;
+//console.log(urlcourante)
+
+
+let url = new URL(urlcourante)
+let search_params = new URLSearchParams(url.search);
+let ids = search_params.get('id');
+
+
+const oneData = () => {
+    axios.get('http://localhost:3000/api/teddies/' + ids)
+        .then(response => {
+            let idBear = response.data._id
+            let nameBear = response.data.name
+            let imgBear = response.data.imageUrl
+            let priceBear = response.data.price
+            let descritptionBear = response.data.description
+            console.log(idBear, nameBear, imgBear, priceBear, descritptionBear)
+            document.querySelector('#title').innerHTML = nameBear
+            document.querySelector('.img-fluid').src = imgBear
+            document.querySelector('#price').innerHTML = priceBear
+            document.querySelector('#description').innerHTML = descritptionBear
         })
 }
 
-getData();
+oneData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
