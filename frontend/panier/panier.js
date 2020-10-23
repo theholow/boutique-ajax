09c5c1aa458b82574ee2613882127f3140488c39
unknown <owen.guerin121@gmail.com>
2020-10-23 14:19:56 +0200

    Bouton delete

--- frontend/panier/panier.js
+++ frontend/panier/panier.js
@@ -7,6 +7,7 @@ let panierParsed = JSON.parse(panier)
 /*Affichage des produits dans le panier*/
 for (const produit of panierParsed) {
     getDataForBasketDisplay(produit.id, produit.quantite, produit.color)
+
 }
 
 /* Recuperation des données de l'API*/
@@ -20,26 +21,64 @@ function getDataForBasketDisplay(idProduct, quantiteBear, couleur) {
             let descritptionBear = response.data.description
             let colors = response.data.color
 
-            document.querySelector('.article-container').innerHTML += displayCommande(idProduct,imgBear,nameBear,couleur,quantiteBear,priceBear)
+            document.querySelector('.article-container').innerHTML += displayCommande(idProduct, imgBear, nameBear, couleur, quantiteBear, priceBear)
+            let deleteButton = document.querySelector('.btn')
+
+            deleteButton.addEventListener('click', function () {
+                console.log('coucou')
+                removeFromBasket(this.dataset.id, panierParsed)
+            })
+
         })
 }
 
 /*Affichage de chaque produit*/
-function displayCommande(id,img, name,color, quantite, price) {
+function displayCommande(id, img, name, color, quantite, price) {
     return '<div class="article container-fluid">' +
         '<img class="product-image" src="' + img + '">' +
         '<h3 class="product-name">"' + name + '"</h3>' +
-        '<h5 class="product-color">"' + color + '"</h5>'+
+        '<h5 class="product-color">"' + color + '"</h5>' +
         '<h4 class="product-price">"' + quantite + '"</h4>' +
-        '<h5 class="product-quantity">"' + price + '"</h5>'+
-        `<button class="btn btn-danger" data-id="${id+color}">Supprimer</button>`+
+        '<h5 class="product-quantity">"' + price + '"</h5>' +
+        `<button class="btn btn-danger" data-id="${id + string_to_slug(color)}">Supprimer</button>` +
         '</div>'
+
+
+
 }
 
-let deleteButton = document.querySelector('.btn-danger')
 
-deleteButton.addEventListener('click', removeFromBasket)
+function string_to_slug (str) {
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
 
-function removeFromBasket(panier){
+    // remove accents, swap ñ for n, etc
+    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+    var to   = "aaaaeeeeiiiioooouuuunc------";
+    for (var i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
+
+    return str;
+}
+
+
+
+//Enlève le produit du panier (bêta)
+function removeFromBasket(dataId, panier) {
+    let id = dataId.substr(0, 23)
+    let color = dataId.substr(24)
+
+    for (let i = 0; i < panier.length; i++) {
+        if (dataId === i.id && dataId === i.color) {
+            panier.splice[i, 1];
+            purchase(panier)
+            break;
+        }
+    }
 
 }
